828e855b2a403eedc38c33ac0a593683b66e03e2
Michaelxuzhi <michaelxuzhi@foxmail.com>
2018-10-02 11:44:05 +0800

    Add files via upload

---
+++ yuo.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+int main()
+{
+	int a,b,c,d,e,f,s=4;
+	a=1,b=2,c=3;
+	d=a+b;
+	e=a+c;
+	f=b+c;
+	if(s==d)
+	{
+		cout<<"LOL"<<endl;
+	}
+	else if(s==e)
+	{
+		cout<<"NICE"<<endl;
+	}
+	else if(s==f)
+	{
+		cout<<"HAHA"<<endl;
+	}
+	return 0;
+}
